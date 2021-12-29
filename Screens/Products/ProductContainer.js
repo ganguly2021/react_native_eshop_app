c0fa203bf15c9284084d549db8d0fb8d478cfd71
radeon <radeon@abc.com>
2021-12-29 13:00:41 +0530

    product container setup.

---
+++ Screens/Products/ProductContainer.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { View, StyleSheet, ActivityIndicator, Text } from "react-native";
+
+function ProductContainer() {
+  return (
+    <View style={styles.container}>
+      <Text>Product Container.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default ProductContainer;
