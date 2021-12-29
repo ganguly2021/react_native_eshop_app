6a853152a0de559727639a6e3193bae8ab583e7f
radeon <radeon@abc.com>
2021-12-29 13:40:01 +0530

    product items in flat list setup.

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,16 +1,49 @@
-import React, { useState } from "react";
-import { View, StyleSheet, ActivityIndicator, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  Text,
+  FlatList,
+} from "react-native";
+
+// import products data
+const data = require("./../../products.json");
 
 function ProductContainer() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(data);
+
+    // component unmount
+    return () => {
+      setProducts([]);
+    };
+  }, []);
+
+  console.log(data);
+
   return (
     <View style={styles.container}>
       <Text>Product Container.</Text>
+      <View style={styles.list}>
+        <FlatList
+          horizontal
+          data={products}
+          renderItem={({ item }) => <Text>{item.brand}</Text>}
+          keyExtractor={(item) => item.name}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {},
+  list: {
+    marginTop: 100,
+  },
 });
 
 export default ProductContainer;
